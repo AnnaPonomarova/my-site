eb4339e31b34847372f17403c2fe86ce545808d5
AnnaPonomarova <74797655+AnnaPonomarova@users.noreply.github.com>
2020-11-21 22:03:36 +0200

    siteWAY

---
+++ js/main.js
@@ -0,0 +1,37 @@
+$(function () {
+	$(window).scroll(function() {
+	    $('.section-title').each(function(){
+	        var imagePos = $(this).offset().top;
+
+	        var topOfWindow = $(window).scrollTop();
+	        if (imagePos < topOfWindow+650) {
+	            $(this).addClass("wobble");
+	        }
+	    });
+	});
+
+	$(window).scroll(function() {
+	    $('.options').each(function(){
+	        var imagePos = $(this).offset().top;
+
+	        var topOfWindow = $(window).scrollTop();
+	        if (imagePos < topOfWindow+650) {
+	            $(this).addClass("fadeInUp");
+	        }
+	    });
+	});
+
+
+	$(window).scroll(function() {
+	    $('.ticket').each(function(){
+	        var imagePos = $(this).offset().top;
+
+	        var topOfWindow = $(window).scrollTop();
+	        if (imagePos < topOfWindow+650) {
+	            $(this).addClass("jello");
+	        }
+	    });
+	});
+
+
+})
